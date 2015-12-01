09cf7f46f574e89a88cdbb1312b92fc28c878d9d
keetian <876981515@qq.com>
2015-12-01 10:40:42 +0800

    Update common.js

--- common.js
+++ common.js
@@ -39,8 +39,64 @@ function timer(intDiff){
 
 timer(intDiff);
 
-//more...
 
+/**
+* author zlz
+* date 20150821
+* function 检查手机号格式是否正确
+* @param  {num} 手机号字符串
+* @return int {0：num为空；1: 格式有误；2：验证通过}
+*/
+var checkPhone = function(phone) {
+  var phoneReg = /^(13[0-9]|14[0-9]|15[0-9]|18[0-9]|17[0-9])\d{8}$/i;
+  var result = false; //phone为空
+  if (phoneReg.test(phone)) {
+      result = true;
+  }
+  return result;
+};
+
+/**
+* author zlz
+* date 20150821
+* function 检查值是否大于指定长度
+* @param {[string]} [str] [检测值]
+* @param {[int]} [len] [指定长度]
+* @return int {0：为空；1: 不为空}
+*/
+var checkValLength = function(str, len) {
+  var result = false;
+  if (str.length > len - 1) {
+      result = true;
+  }
+  return result;
+}
+
+/**
+* author zlz
+* date 20150821
+* function 检查值是否为空
+* @param  {[string]} [val] [检测值]
+* @return int {0：为空；1: 不为空}
+*/
+var checkIsNull = function(val) {
+  var result = false;
+  if (val == "" || val == null || val == undefined || val == "" || val.length == 0) {
+      result = true;
+  }
+  return result;
+};
+
+/**
+* author keetian
+* date 20151121
+* function 个位数字补零
+* @param n为数字
+* @return 返回值为字符串
+*/
+var toDub = function(n) {
+  return n < 10 ? '0' + n : '' + n;
+};
 
 
 
